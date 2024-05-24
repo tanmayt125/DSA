9bdbb918ad8d46ed9587b5d3cdf79a248f972534
Tanmay Tiwari <tanmaytiwari0508@gmail.com>
2024-05-24 13:54:50 +0530

    kernal handling

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,8 @@
 using namespace std;
 
 int main(){
+    // std::cout operates entirely in user space, managing the data and buffering it before making system calls to the kernel.
+    // The kernel handles the actual writing of the data to the console or other output devices, 
+    // but the high-level operations of std::cout are within the domain of user space.
     cout<<"Hello World";
 }
